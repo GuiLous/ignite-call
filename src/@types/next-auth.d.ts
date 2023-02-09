ef1dcecc895e7c69b4d41bab7ef33bb99ec10cfe
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-02-09 09:08:20 -0300

    feat: finalização do form de horários
    Formulários de disponibilidades de horáriso finalizado e persistindo no banco

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -1,13 +1,17 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import NextAuth from 'next-auth/next'
 
 declare module 'next-auth' {
-  // eslint-disable-next-line no-unused-vars
   interface User {
     id: string
     name: string
     email: string
     username: string
     avatar_url: string
+  }
+
+  interface Session {
+    user: User
   }
 }
