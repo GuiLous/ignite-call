a4dbefd4838d9deb0f0ba6650a24a69c950d255a
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-02-14 09:19:12 -0300

    feat: estrutura do calendario
    Estilização inicial do componente de calendário

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,12 +1,17 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
     .map((weekday) => {
-      return weekday
-        .substring(0, 1)
-        .toLocaleUpperCase()
-        .concat(weekday.substring(1))
+      if (short) {
+        return weekday.substring(0, 3).toUpperCase()
+      }
+
+      return weekday.substring(0, 1).toUpperCase().concat(weekday.substring(1))
     })
 }
